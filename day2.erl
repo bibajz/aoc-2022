23bb32c18bb22ab4b68a57cc72b6ae14e3e998ec
bibajz <libasmartinek@protonmail.com>
2022-12-02 20:02:44 +0100

    Day 2, part 2 solution - Erlang

--- day2.erl
+++ day2.erl
@@ -1,6 +1,6 @@
 -module(day2).
 
--export([solution_pt1/0, parse_line/1]).
+-export([solution_pt1/0, solution_pt2/0]).
 
 read_lines(FileName) ->
   case file:read_file(FileName) of
@@ -27,11 +27,37 @@ turn({Move1, Move2}) ->
     end
   end.
 
-score_move(Move) ->
+turn_pt2({Move1, Move2}) ->
+  case Move2 of
+    <<"X">> -> {loss, case Move1 of
+      <<"A">> -> <<"C">>;
+      <<"B">> -> <<"A">>;
+      <<"C">> -> <<"B">>
+    end};
+    <<"Y">> -> {draw, case Move1 of
+      <<"A">> -> <<"A">>;
+      <<"B">> -> <<"B">>;
+      <<"C">> -> <<"C">>
+    end};
+    <<"Z">> -> {win, case Move1 of
+      <<"A">> -> <<"B">>;
+      <<"B">> -> <<"C">>;
+      <<"C">> -> <<"A">>
+    end}
+  end.
+
+score_move_pt1(Move) ->
   case Move of
         <<"X">> -> 1;
         <<"Y">> -> 2;
         <<"Z">> -> 3
+  end.
+
+score_move_pt2(Move) ->
+  case Move of
+        <<"A">> -> 1;
+        <<"B">> -> 2;
+        <<"C">> -> 3
   end.
 
 score_result(Result) ->
@@ -41,8 +67,11 @@ score_result(Result) ->
         loss -> 0
   end.
 
-score_turn({Result, Move}) ->
-  score_move(Move) + score_result(Result).
+score_turn_pt1({Result, Move}) ->
+  score_move_pt1(Move) + score_result(Result).
+
+score_turn_pt2({Result, Move}) ->
+  score_move_pt2(Move) + score_result(Result).
 
 parse_line(Line) ->
   [L,R|_] = string:split(Line, " "),
@@ -51,7 +80,13 @@ parse_line(Line) ->
 solution_pt1() ->
   {ok, Lines} = read_lines("input/day2.txt"),
   lists:sum(
-    lists:map(fun(S) -> score_turn(S) end,
+    lists:map(fun(S) -> score_turn_pt1(S) end,
       lists:map(fun(S) -> turn(S) end,
       lists:map(fun(S) -> parse_line(S) end, Lines)))).
 
+solution_pt2() ->
+  {ok, Lines} = read_lines("input/day2.txt"),
+  lists:sum(
+    lists:map(fun(S) -> score_turn_pt2(S) end,
+      lists:map(fun(S) -> turn_pt2(S) end,
+      lists:map(fun(S) -> parse_line(S) end, Lines)))).
