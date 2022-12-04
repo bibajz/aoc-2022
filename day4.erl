b3910e00cf97a048eb72b21914238c35aaeac366
bibajz <libasmartinek@protonmail.com>
2022-12-04 14:14:27 +0100

    Day 4, part 2 solution - Erlang

--- day4.erl
+++ day4.erl
@@ -1,6 +1,6 @@
 -module(day4).
 
--export([read_lines/1, seq_from_range/1, are_overlapping/2, solution_pt1/0]).
+-export([read_lines/1, seq_from_range/1, are_overlapping/2, solution_pt1/0, solution_pt2/0]).
 
 read_lines(FileName) ->
   case file:read_file(FileName) of
@@ -17,6 +17,10 @@ are_overlapping(List1, List2) ->
   {S1, S2} = {sets:from_list(List1), sets:from_list(List2)},
   sets:is_subset(S1, S2) or sets:is_subset(S2, S1) or false.
 
+have_common_element(List1, List2) ->
+  {S1, S2} = {sets:from_list(List1), sets:from_list(List2)},
+  not sets:is_empty(sets:intersection([S1, S2])).
+
 bool_to_int(Bool) ->
   case Bool of
     true -> 1;
@@ -30,5 +34,15 @@ solution_pt1() ->
     lists:map(
     fun(B) -> bool_to_int(B) end,
     lists:map(fun({LSeq, RSeq}) -> are_overlapping(LSeq, RSeq) end,
+    lists:map(fun({LString, RString}) -> {seq_from_range(LString), seq_from_range(RString)} end,
+      lists:map(fun(L) -> [LString,RString|_] = string:split(L, ",", all), {LString, RString} end, Lines))))).
+
+
+solution_pt2() ->
+  {ok, Lines} = read_lines("input/day4.txt"),
+  lists:sum(
+    lists:map(
+    fun(B) -> bool_to_int(B) end,
+    lists:map(fun({LSeq, RSeq}) -> have_common_element(LSeq, RSeq) end,
     lists:map(fun({LString, RString}) -> {seq_from_range(LString), seq_from_range(RString)} end,
       lists:map(fun(L) -> [LString,RString|_] = string:split(L, ",", all), {LString, RString} end, Lines))))).
